9c03108e3a67c9dda3969b052fe71510996dff29
kcbighuge <kcbighuge@gmail.com>
2017-07-18 14:05:48 -0700

    update init & prediction functions

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -20,32 +20,91 @@
 using namespace std;
 
 void ParticleFilter::init(double x, double y, double theta, double std[]) {
-	// TODO: Set the number of particles. Initialize all particles to first position (based on estimates of 
+	// TO_DID: Set the number of particles. Initialize all particles to first position (based on estimates of 
 	//   x, y, theta and their uncertainties from GPS) and all weights to 1. 
 	// Add random Gaussian noise to each particle.
 	// NOTE: Consult particle_filter.h for more information about this method (and others in this file).
 
+	num_particles = 80;
+
+	default_random_engine gen;
+
+	normal_distribution<double> N_x(x, std[0]);
+	normal_distribution<double> N_y(y, std[1]);
+	normal_distribution<double> N_theta(theta, std[2]);
+
+	for (int i=0; i<num_particles; ++i) {
+
+		Particle particle;
+		particle.id = i;
+		particle.x = N_x(gen);
+		particle.y = N_y(gen);
+		particle.theta = N_theta(gen);
+		particle.weight = 1;
+
+		particles.push_back(particle);
+		weights.push_back(1);
+
+	}
+
+  is_initialized = True;
+
 }
 
 void ParticleFilter::prediction(double delta_t, double std_pos[], double velocity, double yaw_rate) {
-	// TODO: Add measurements to each particle and add random Gaussian noise.
+	// TO_DID: Add measurements to each particle and add random Gaussian noise.
 	// NOTE: When adding noise you may find std::normal_distribution and std::default_random_engine useful.
 	//  http://en.cppreference.com/w/cpp/numeric/random/normal_distribution
 	//  http://www.cplusplus.com/reference/random/default_random_engine/
+	
+	default_random_engine gen;
+
+	for (int i=0; i<num_particles; ++i) {
+
+		double new_x;
+		double new_y;
+		double new_theta;
+
+		if (yaw_rate == 0) {
+			new_x = particles[i].x + velocity*delta_t * cos(particles[i].theta);
+			new_y = particles[i].y + velocity*delta_t * sin(particles[i].theta);
+			new_theta = particles[i].theta;
+		} else {
+			new_x = particles[i].x + velocity/yaw_rate * ( sin(particles[i].theta + yaw_rate*delta_t) - sin(particles[i].theta) );
+			new_y = particles[i].y + velocity/yaw_rate * ( cos(particles[i].theta) - cos(particles[i].theta + yaw_rate*delta_t) );
+			new_theta = particles[i].theta + yaw_rate*delta_t;
+		}
+		
+		normal_distribution<double> N_x(new_x, std_pos[0]);
+	    normal_distribution<double> N_y(new_y, std_pos[1]);
+	    normal_distribution<double> N_theta(new_theta, std_pos[2]);
+
+	    particles[i].x = N_x(gen);
+	    particles[i].y = N_y(gen);
+	    particles[i].theta = N_theta(gen);
+
+	}
 
 }
 
 void ParticleFilter::dataAssociation(std::vector<LandmarkObs> predicted, std::vector<LandmarkObs>& observations) {
-	// TODO: Find the predicted measurement that is closest to each observed measurement and assign the 
+	// TODOING: Find the predicted measurement that is closest to each observed measurement and assign the 
 	//   observed measurement to this particular landmark.
 	// NOTE: this method will NOT be called by the grading code. But you will probably find it useful to 
 	//   implement this method and use it as a helper during the updateWeights phase.
+	for (int i=0; i<predicted.size(); ++i) {
+
+		for (int j=0; i<observations.size(); ++i) {
+			closest = predicted[i] - observations[j];
+		}
+
+	}
 
 }
 
 void ParticleFilter::updateWeights(double sensor_range, double std_landmark[], 
 		std::vector<LandmarkObs> observations, Map map_landmarks) {
-	// TODO: Update the weights of each particle using a mult-variate Gaussian distribution. You can read
+	// TODOING: Update the weights of each particle using a mult-variate Gaussian distribution. You can read
 	//   more about this distribution here: https://en.wikipedia.org/wiki/Multivariate_normal_distribution
 	// NOTE: The observations are given in the VEHICLE'S coordinate system. Your particles are located
 	//   according to the MAP'S coordinate system. You will need to transform between the two systems.
@@ -55,18 +114,52 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
 	//   and the following is a good resource for the actual equation to implement (look at equation 
 	//   3.33
 	//   http://planning.cs.uiuc.edu/node99.html
+
+	vector<LandmarkObs> trans_observations;
+
+  for (int p=0; i<num_particles; ++i) {
+
+  	trans_observations[i] = ;
+
+    for  (int i=0; j<map_landmarks.size(); ++j) {
+      
+      association = dataAssociation(map_landmarks[i], trans_observations[i]);
+
+		  if (association !=0) {
+			  double meas_x = trans_observations[i].x;
+			  double meas_y = trans_observations[i].y;
+
+			  double mu_x = map_landmarks.landmark_list[association].x_f;
+			  double mu_y = map_landmarks.landmark_list[association].y_f;
+
+        double a = pow(meas_x-mu_x, 2) / (2 * pow(std_landmark[0], 2));
+	      double b = pow(meas_y-mu_y, 2) / (2 * pow(std_landmark[1], 2));
+			  long long multiplier = 1 / (2*M_PI * std_landmark[0]*std_landmark[1]) * exp(-(a+b));
+
+			  if (multiplier>0) {
+			  	particles[p].weight *= multiplier;
+			  }
+			}
+			associations.push_back(association+1);
+			sense_x.push_back(trans_observations[i].x);
+			sense_y.push_back(trans_observations[i].y);
+    }
+
+  }
+
 }
 
 void ParticleFilter::resample() {
-	// TODO: Resample particles with replacement with probability proportional to their weight. 
+	// TO_DID: Resample particles with replacement with probability proportional to their weight. 
 	// NOTE: You may find std::discrete_distribution helpful here.
 	//   http://en.cppreference.com/w/cpp/numeric/random/discrete_distribution
-	std::default_random_engine gen;
-	std::discrete_distribution<int> distribution(weights.begin(), weights.end());
+	
+	default_random_engine gen;
+	discrete_distribution<int> distribution(weights.begin(), weights.end());
 
 	vector<Particle> resample_particles;
 
-	for (int i=0; i < num_particles; i++)	{
+	for (int i=0; i < num_particles; ++i)	{
 		resample_particles.push_back(particles[distribution(gen)]);
 	}
 
